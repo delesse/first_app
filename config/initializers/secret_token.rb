60557850b7ce7414d90328de44b0699284976a76
root <root@s15333141.onlinehome-server.info>
2011-09-06 19:29:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'dcfd4631e637efc1b73994d096fdcb2f96b22f21bc6ab5872f6dbc61ac41f20dd7a60b1ec435dc1c346ded03126b08e744e4c032189dca2034bdcb4e82fac112'
